960c5bd50f5b998ad3b519f7c51cfd6f21d28f39
Bernardo Robaina <bernardo.robaina@serel.com.br>
2025-05-25 19:52:10 -0300

    FeitoEmAula

---
+++ feitoEmAula.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Versão feita em aula - 23/05/2025
+
+char* longestNiceSubString(char* s){
+    char* result;
+    char alphabet[strlen(s)];
+    int tmpIndex = 0;
+
+    int strlenAlphabet = sizeof(alphabet)/sizeof(char);
+
+    for(int i = 0; i < strlen(s); i++){
+        if(i == 0){
+            alphabet[tmpIndex] = s[i];
+        }else{
+            bool insideAlphabet = false;
+            printf("%d", strlen(alphabet));
+            for(int j = 0; j < strlen(alphabet); j++){
+                if(s[i] == alphabet[j] || s[i] == (alphabet[j] + 32) || s[i] == (alphabet[j] - 32)){
+                    insideAlphabet = true;
+                }
+            }
+            if(!insideAlphabet){
+                alphabet[++tmpIndex] = s[i];
+            }
+        }
+    }
+
+    for(int i = 0; i < sizeof(alphabet)/sizeof(char); i++){
+        printf("%c", alphabet[i]);
+    }
+}
+
+void main(){
+    
+    char* s = "YazaAay";
+    printf("String em teste: %s", s);
+
+    longestNiceSubString(s);
+}
