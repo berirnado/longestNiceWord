4e7889d8952c8eaa7ea5756257e6eb9f65b083ec
Bernardo Robaina <bernardo.robaina@serel.com.br>
2025-05-25 19:22:08 -0300

    Primeira versão que funcionou (brute force cada substring)

--- main.c
+++ main.c
@@ -3,38 +3,72 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-char* longestNiceSubString(char* s){
-    char* result;
-    char alphabet[strlen(s)];
-    int tmpIndex = 0;
+//Função que vai percorrer cada letra a partir de um endereço informado, pelo numero de letras informado
+//o endereço informado vai ser o inicio da substring
+//o numero de letras vai ser o limite da substring
+//para cada letra, vai entrar em um for procurando seu par
+bool isNiceSubStrig(char* str, int stringSize){
+    //for que itera por cada letra 
+    for(int i = 0; i < stringSize; i++){
+        bool matchFound = false;
 
-    int strlenAlphabet = sizeof(alphabet)/sizeof(char);
+        for(int j = 0; j < stringSize; j++){
+            //pega letra x da substring e procura por um par pra ela em cada casa da substring
+            if(str[i] == (str[j] + 32) || str[i] == (str[j] - 32)){
+                matchFound = true;
+            }
+        }
+
+        //caso nao encontre o par de apenas uma das letras já era
+        if(!matchFound) return false;
+    }
+
+    //se chegou aqui é pq encontrou um par para todas as letras da substring, configurando niceString
+    return true;
+}
+
+char* longestNiceSubString(char* s){
+    int longestSize = 0;
+    char* longestSubString = NULL; 
 
     for(int i = 0; i < strlen(s); i++){
-        if(i == 0){
-            alphabet[tmpIndex] = s[i];
-        }else{
-            bool insideAlphabet = false;
-            printf("%d", strlen(alphabet));
-            for(int j = 0; j < strlen(alphabet); j++){
-                if(s[i] == alphabet[j] || s[i] == (alphabet[j] + 32) || s[i] == (alphabet[j] - 32)){
-                    insideAlphabet = true;
-                }
+        for(int j = strlen(s); j > i; j--){
+            int subStringSize = j - i;
+            char* substring = (char*)malloc((subStringSize + 1) * sizeof(char));
+            
+            if(substring == NULL) {
+                printf("\nErro ao alocar memória.");
+                exit(0);
             }
-            if(!insideAlphabet){
-                alphabet[++tmpIndex] = s[i];
+
+            //gera substring a partir da letra iterada
+            for(int q = 0; q < subStringSize; q++){
+                substring[q] = s[q + i];
             }
+            substring[subStringSize] = '\0';
+
+            //testa se a subString em questão é niceString
+            //se for uma niceString e seu tamanho for maior do que o maior tamanho de nicestring já registrado
+            if(isNiceSubStrig(substring, subStringSize) && subStringSize > longestSize){
+                //libera antigo maior niceString
+                if(longestSubString != NULL) free(longestSubString);
+
+                //atualiza os valores de maior nice substring já encontrada
+                longestSize = subStringSize;
+                longestSubString = substring;
+            }else{
+                free(substring);
+            }
+            
         }
     }
 
-    for(int i = 0; i < sizeof(alphabet)/sizeof(char); i++){
-        printf("%c", alphabet[i]);
-    }
+    return longestSubString;
 }
 
 void main(){
-    char* s = "YazaAay";
+    char* s = "c";
     printf("String em teste: %s", s);
 
-    longestNiceSubString(s);
+    printf("\n%s", longestNiceSubString(s));
 }
